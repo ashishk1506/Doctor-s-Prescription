9b50cd7ea618a062089e0e8436de551ae79a7b02
ashishk1506 <2019ugec002@nitjsr.ac.in>
2022-01-31 15:06:02 +0530

    Changed port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const cors = require("cors");
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT;
 const presRoutes = require("./routes/routes")
 
 //middlewares
