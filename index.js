d20e308aa39d652e59b565b65d7e3a74372da3fd
Ashish Kumar <ashishkumar@Ashishs-MacBook-Pro.local>
2022-10-21 01:33:05 +0530

    Delete port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const cors = require("cors");
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 const presRoutes = require("./routes/routes")
 
 //middlewares
